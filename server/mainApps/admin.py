c0b3f47e0878069f8594fc35905fb0e35c9583d4
Miha <xd383727@gmail.com>
2021-12-01 22:53:17 +0300

    create models

---
+++ server/mainApps/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Office)
+admin.site.register(Instructions)
+admin.site.register(Posts)
---
+++ server/mainApps/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MainappsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mainApps'
+    verbose_name='Главная'
