050ff6328a3d01a59c12b8cb439ebae319fd39a9
Miha <xd383727@gmail.com>
2021-12-07 00:52:21 +0300

    create register user

--- server_python/restApp/serializers.py
+++ server_python/restApp/serializers.py
@@ -3,24 +3,42 @@ from mainApps.models import Instructions, Office, Person, Posts
 
 
 class InstructionsSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Instructions
-		fields = "__all__"
+    class Meta:
+        model = Instructions
+        fields = "__all__"
+
 
 class PostSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Posts
-		fields = ('title', 'instuctions', 'allowance', 'allowance_level')
+    class Meta:
+        model = Posts
+        fields = ('title', 'instuctions', 'allowance', 'allowance_level')
+
 
 class OfficeSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Office
-		fields = "__all__"
+    class Meta:
+        model = Office
+        fields = "__all__"
 
 
 class PersonSerializer(serializers.ModelSerializer):
-	post = PostSerializer(many=False)
-	office = OfficeSerializer(many=False)
-	class Meta:
-		model = Person
-		fields = ("first_name", "first_name", "post", "allowance", "office")
+    post = PostSerializer(many=False)
+    office = OfficeSerializer(many=False)
+    password2 = serializers.CharField()
+    email = serializers.EmailField()
+    def save(self, **kwargs):
+        user = Person(
+            email=self.validated_data['email'],
+            username=self.validated_data['username'])
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError({password: "Пароли не совпадают"})
+        user.set_password(password)
+        user.save()
+        return user
+
+    class Meta:
+        model = Person
+        fields = ('username', 'password', 'password2', 'email', "first_name", "last_name", "post", "allowance", "office")
+
+
--- server_python/restApp/urls.py
+++ server_python/restApp/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from .views import InstructionsViews, PersonSerializer
+from django.urls import path, include
+from .views import InstructionsViews, CreatePerson
 
 
 urlpatterns = [
     path('instruction/', InstructionsViews.as_view()),
-    path('user_list/', InstructionsViews.as_view())
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('register/', CreatePerson.as_view())
 ]
